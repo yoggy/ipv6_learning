81f49e6ea407bdb53d476b263a19a8ca41973456
yoggy <yoggy0@gmail.com>
2013-07-11 20:41:57 +0900

    modified:   echo_server.c

--- echo_server.c
+++ echo_server.c
@@ -23,7 +23,7 @@ int main(int argc, char *argv[])
 	int  read_size;
 
 	memset((void*)(&hints), 0, sizeof(struct addrinfo));
-	hints.ai_family   = AF_INET6;
+	hints.ai_family   = AF_INET6;  // can accept IPv4 address
 	hints.ai_socktype = SOCK_STREAM;
 	hints.ai_flags    = AI_PASSIVE;
 
